c6b9e36b6c8131fbe8594e27641fa93a155e69af
WAHU0508 <sharongikenye@gmail.com>
2024-06-29 09:07:16 +0300

    Add comments

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your solution here!
+//destructively append a cat's name
 const cats = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name) {
     return cats.push(name);
@@ -6,40 +7,47 @@ function destructivelyAppendCat(name) {
 destructivelyAppendCat("Raph");
 console.log(cats);
 
+//destructively prepend a cat's name
 function destructivelyPrependCat(name) {
     return cats.unshift(name);
 }
 destructivelyPrependCat("Bob");
 console.log(cats);
 
+//destructively remove last cat's name
 function destructivelyRemoveLastCat() {
     return cats.pop();
 }
 destructivelyRemoveLastCat();
 console.log(cats);
 
+//destructively remove first cat's name
 function destructivelyRemoveFirstCat() {
     return cats.shift();
 }
 destructivelyRemoveFirstCat();
 console.log(cats);
 
+//non-destructively append a cat's name
 function appendCat(name) {
     return [...cats, name];
 }
 console.log(appendCat("Broom"));
 
+//non-destructively prepend a cat's name
 function prependCat(name) {
     return [name, ...cats];
 }
 console.log(prependCat("Arnold"));
 
+//non-dstructively remove last cat's name
 function removeLastCat() {
     return cats.slice(0, 2)
 }
 removeLastCat();
 console.log(removeLastCat());
 
+//non-destructively remove first cat's name
 function removeFirstCat() {
     return cats.slice(1);
 }
